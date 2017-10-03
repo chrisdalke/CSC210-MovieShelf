69970d55955c0d93697635d507327028e582b02a
Chris Dalke <chrisd@beco.io>
2017-10-02 22:21:05 -0400

    Added simple user authentication test.

---
+++ Development/src/main/java/com/grup/movieshelf/Controller/Web/IndexController.java
@@ -0,0 +1,14 @@
+package com.grup.movieshelf.Controller.Web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String testWeb(Model model){
+        return "index";
+    }
+}
