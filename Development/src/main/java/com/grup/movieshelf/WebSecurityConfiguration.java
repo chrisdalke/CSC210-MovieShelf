a551465af63271e5d82c3039b4032fb3673e3745
Chris Dalke <chrisd@beco.io>
2017-10-02 21:16:12 -0400

    Added some bare minimum config and pages to test the project.

---
+++ Development/src/main/java/com/grup/movieshelf/WebSecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.grup.movieshelf;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .anyRequest().permitAll(); // For now, permit any request. We can change this later
+    }
+
+    // Defines where login authentication is checked against. For now this is a placeholder.
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+}
