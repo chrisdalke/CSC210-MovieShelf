d8a71efa37677696be5776a59d198ce8507b67c7
Chris Dalke <chrisd@beco.io>
2017-10-04 20:14:11 -0400

    Updated web security configuration to ignore static files.

--- Development/src/main/java/com/grup/movieshelf/WebSecurityConfiguration.java
+++ Development/src/main/java/com/grup/movieshelf/WebSecurityConfiguration.java
@@ -1,11 +1,14 @@
 package com.grup.movieshelf;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /*
 TODO READ
@@ -15,25 +18,30 @@ https://hellokoding.com/registration-and-login-example-with-spring-security-spri
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        /*
         http
                 .authorizeRequests()
-                    .anyRequest().permitAll(); // For now, permit any request. We can change this later
-                    */
-
-        http
-                .authorizeRequests()
-                .antMatchers("/").permitAll()
-                .anyRequest().authenticated()
-                .and()
+                    .antMatchers("/").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
                 .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+                    .loginPage("/login").permitAll()
+                    .and()
+                    .logout().permitAll();
+    }
+
+    // Ignore static files
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/css/**", "/img/**", "/js/**", "/fonts/**");
     }
 
     // Defines where login authentication is checked against. For now this is a placeholder.
