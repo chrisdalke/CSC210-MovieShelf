0ac24c6f61b96e7b6e647c68dc58dbcb2329869e
Chris Dalke <chrisdalke@gmail.com>
2017-10-10 23:09:40 -0400

    Updating some of the HTML fragments.

--- Development/src/main/java/com/grup/movieshelf/Controller/Web/IndexController.java
+++ Development/src/main/java/com/grup/movieshelf/Controller/Web/IndexController.java
@@ -4,13 +4,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
 
     @RequestMapping("/")
-    public String testWeb(Model model){
+    public String testWeb(HttpServletRequest request, Model model){
 
-        model.addAttribute("value","Hello World");
+        if (request.getUserPrincipal() == null) {
+
+            model.addAttribute("title", "Not Logged In");
+        } else {
+            model.addAttribute("title", "Logged In");
+        }
 
         return "index";
     }
