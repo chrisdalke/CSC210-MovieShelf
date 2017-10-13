095c548bdeecf393939785b0deff071e49c948aa
Chris Dalke <chrisdalke@gmail.com>
2017-10-13 11:22:33 -0400

    Combined user controllers into a single file so it's easier to understand.

---
+++ Development/src/main/java/com/grup/movieshelf/Controller/Web/UserController.java
@@ -0,0 +1,24 @@
+package com.grup.movieshelf.Controller.Web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/login")
+    public String userLoginEndpoint(Model model){
+        return "login";
+    }
+
+    @RequestMapping("/user/options")
+    public String userOptionsEndpoint(Model model){
+        return "userOptions";
+    }
+
+    @RequestMapping("/user/register")
+    public String userRegisterEndpoint(Model model){
+        return "userRegister";
+    }
+}
