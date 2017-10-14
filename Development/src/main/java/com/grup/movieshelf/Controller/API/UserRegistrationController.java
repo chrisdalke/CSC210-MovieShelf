6c7d43dfdcff291e18bcaf1430cdd4948b3a400e
danielstegink <danielstegink@yahoo.com>
2017-10-14 10:22:44 -0400

    Update UserRegistrationController.java

--- Development/src/main/java/com/grup/movieshelf/Controller/API/UserRegistrationController.java
+++ Development/src/main/java/com/grup/movieshelf/Controller/API/UserRegistrationController.java
@@ -12,17 +12,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserRegistrationController {
-    //should handle user login
-    //link to account creation page
-    //when a user tries to make an account, they input a username, password, and some other info
-    //info is used to make a User object, which is stored on the SQL database
-        //need a username, a password, and an id
-            //can also have miscellaneous information for flavor?
-            //include a password encryptor?
-            //randomly generate a user id? hash function? id = hash of username and/or password?
         //role - needed for certain authorities; how to implement? do we even need to for account creation
             //should make officer accounts manually? accounts made user by defaul and then given permissions by admin?
-    // how to push User objects to UserRepository?
 
     @Autowired
     private UserRepository userRepository;
@@ -44,12 +35,11 @@ public class UserRegistrationController {
         if (userRepository.findByUsername(user.getUsername()) == null)
         {
             hibernateUserDetailsService.saveNewUser(user);
-            model.addAttribute("Account made successfully.");
-            //print out success message - not SOPL, but to .html page?
+            model.addAttribute("message", "Account made successfully.");
         }
         else
         {
-            model.addAttribute("Account creation failed. Invalid username or password.");
+            model.addAttribute("message", "Account creation failed. Username already exists.");
         }
         
         return "userRegister"; //return to a different page? maybe the home page?
