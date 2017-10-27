b5f8a727e19d154cb81b7c71bffe035bf87a7beb
Andrew Gutierrez <AndrewGutierrez@dhcp-10-4-16-11.wireless.rochester.edu>
2017-10-26 22:59:31 -0400

    Configured to add grid elements for each row. Will fix bug with empty first column.

--- Shelf Script Testing/js/movieShelf.js
+++ Shelf Script Testing/js/movieShelf.js
@@ -2,6 +2,7 @@ var script = document.createElement('script');
 script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
 script.type = 'text/javascript';
 
+/*
 function addToGrid() {
     var grid=$("#grid");
     console.log(grid);
@@ -11,8 +12,32 @@ function addToGrid() {
     outerDiv.append(card);
     grid.append(outerDiv);
 }
+*/
+
+function addToGrid(grid){
+    var outerDiv=$("<div><div>");
+    var card=$("<div></div>");
+    card.addClass("uk-card uk-card-secondary uk-height-large uk-card-body card");
+    outerDiv.append(card);
+    grid.append(outerDiv);
+}
+
+function addNewGrid(master){
+    var newGrid=$("<div uk-grid><div>");
+    newGrid.addClass("uk-grid-match uk-child-width-1-4 uk-grid-small uk-text-center uk-padding-large grid");
+    newGrid.attr("data-value","1");
+    addToGrid(newGrid);
+    master.append(newGrid);
+}
 
 function addGrid(){
-    var currentGrid=$("#gridElement").last
-    console.log(currentGrid.attr("#value"))
+    var currentGrid=$(".grid:last");
+    var value = parseInt(currentGrid.attr("data-value"));
+    if(value<4){
+        addToGrid(currentGrid);
+        currentGrid.attr("data-value",String(value+1));
+    }else{
+        var masterGrid=$("#gridElement");
+        addNewGrid(masterGrid);
+    }
 }
