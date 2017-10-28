648b86d997865a1efb42d74af7a8978dbcf6881d
Chris Dalke <chrisdalke@gmail.com>
2017-10-28 19:01:36 -0400

    Added blank controllers for when we implement the "Session" functionality for group movie recommendations

---
+++ Development/src/main/java/com/grup/movieshelf/Controller/API/SessionAPIController.java
@@ -0,0 +1,15 @@
+package com.grup.movieshelf.Controller.API;
+
+import com.grup.movieshelf.JPA.Entity.Session;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SessionAPIController {
+
+    @PostMapping
+    public void sessionCreate(){
+
+    }
+
+}
