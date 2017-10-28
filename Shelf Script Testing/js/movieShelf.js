ffa329b7b3e1262a3ff04bf9ba311d894a6e571c
Andrew Gutierrez <AndrewGutierrez@Andrews-MBP-2.home>
2017-10-27 23:07:28 -0400

    Added styling to the grid elements to show the shelf background

--- Shelf Script Testing/js/movieShelf.js
+++ Shelf Script Testing/js/movieShelf.js
@@ -1,23 +1,12 @@
 var script = document.createElement('script');
 script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
 script.type = 'text/javascript';
-
-/*
-function addToGrid() {
-    var grid=$("#grid");
-    console.log(grid);
-    var outerDiv=$("<div><div>");
-    var card=$("<div></div>");
-    card.addClass("uk-card uk-card-secondary uk-height-large uk-card-body card");
-    outerDiv.append(card);
-    grid.append(outerDiv);
-}
-*/
+var list=["inception","Interstellar","Kung-Foo Panda","The Room"];
 
 function addToGrid(grid){
     var outerDiv=$("<div></div>");
     var card=$("<div></div>");
-    card.addClass("uk-card uk-card-secondary uk-height-large uk-card-body card");
+    card.addClass("uk-card uk-card-secondary uk-height-large uk-card-body uk-card-hover card");
     outerDiv.append(card);
     grid.append(outerDiv);
 }
