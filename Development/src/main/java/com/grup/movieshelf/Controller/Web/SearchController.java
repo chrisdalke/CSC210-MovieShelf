8c3c12939952708afeb6e7eba279545508c23901
Nate Conroy <nconroy@u.rochester.edu>
2017-11-01 18:25:08 -0400

    Created SearchController, added basic postmapping for search bar submit

---
+++ Development/src/main/java/com/grup/movieshelf/Controller/Web/SearchController.java
@@ -0,0 +1,32 @@
+package com.grup.movieshelf.Controller.Web;
+
+import com.grup.movieshelf.JPA.Entity.Movies.Title;
+import com.grup.movieshelf.JPA.Repository.TitleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class SearchController {
+
+    @Autowired
+    TitleRepository titleRepository;
+
+    @PostMapping("/search")
+    public String search (Model model, @RequestParam("searchString") String searchString) {
+        System.out.println("User searched \"" + searchString + "\"");
+
+        List<Title> titles = titleRepository.getAllByTitleNameContaining(searchString);
+
+        System.out.println("Results:");
+        for(Title title : titles) {
+            System.out.println(title.getTitleName());
+        }
+
+        return "redirect:/";
+    }
+}
