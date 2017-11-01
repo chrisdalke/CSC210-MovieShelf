be7779f1ddcce9f0984963a7db283cd477b8c56e
Nate Conroy <nconroy@u.rochester.edu>
2017-11-01 18:44:14 -0400

    converted the SearchController to a rest controller called SearchAPIController

--- Development/src/main/java/com/grup/movieshelf/Controller/API/SearchAPIController.java
+++ Development/src/main/java/com/grup/movieshelf/Controller/API/SearchAPIController.java
@@ -1,35 +1,33 @@
-package com.grup.movieshelf.Controller.Web;
+package com.grup.movieshelf.Controller.API;
 
 import com.grup.movieshelf.JPA.Entity.Movies.Title;
 import com.grup.movieshelf.JPA.Repository.TitleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
-public class SearchController {
+@RestController
+public class SearchAPIController {
 
     @Autowired
     TitleRepository titleRepository;
 
     @PostMapping("/search")
-    public String search (Model model, @RequestParam("searchString") String searchString) {
+    public List<Title> search (Model model, @RequestParam("searchString") String searchString) {
         System.out.println("User searched \"" + searchString + "\".");
 
         // TODO: make search bar support not just movie titles (actors, directors, etc.)
         List<Title> titles = titleRepository.getAllByTitleNameContaining(searchString);
-
 
         System.out.println("Returned results:");
         for(Title title : titles) {
             System.out.println(title.getTitleName() + " (" + title.getYear() + ")");
         }
 
-        // TODO: return "/search/results"
-        return "redirect:/";
+        return titles;
     }
 }
