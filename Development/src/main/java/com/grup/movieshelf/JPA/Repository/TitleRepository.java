0dd41a74d89cf65fe4d5e033a0fda7f64a7e177c
Nate Conroy <nconroy@u.rochester.edu>
2017-11-01 15:10:13 -0400

    Added a query method to the TitleRepository for the search implementation

--- Development/src/main/java/com/grup/movieshelf/JPA/Repository/TitleRepository.java
+++ Development/src/main/java/com/grup/movieshelf/JPA/Repository/TitleRepository.java
@@ -3,7 +3,11 @@ package com.grup.movieshelf.JPA.Repository;
 import com.grup.movieshelf.JPA.Entity.Movies.Title;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TitleRepository extends JpaRepository<Title, String> {
 
     Title getByTitleId(String titleId);
+
+    List<Title> getAllByTitleNameContaining(String search);
 }
