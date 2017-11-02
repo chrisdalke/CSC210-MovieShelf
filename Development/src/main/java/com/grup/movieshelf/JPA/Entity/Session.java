87e6d98e8b69cab3107c9e3983649dd83a9b6a7d
Chris Dalke <chrisdalke@gmail.com>
2017-11-01 23:16:07 -0400

    Adding basic session functionality complete with fake recommendations.

--- Development/src/main/java/com/grup/movieshelf/JPA/Entity/Session.java
+++ Development/src/main/java/com/grup/movieshelf/JPA/Entity/Session.java
@@ -1,7 +1,40 @@
 package com.grup.movieshelf.JPA.Entity;
 
-/**
- * Created by chrisdalke on 10/28/17.
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Entity
+@Table(name = "Sessions")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Session {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "sessionId", unique = true, updatable = false)
+    private String titleId;
+
+    @NotNull
+    @Column(name = "isExpired", unique = false, updatable = true)
+    private boolean isExpired;
+
+    @NotNull
+    @Column(name = "sessionCode", unique = true, updatable = true)
+    private String sessionCode;
+
+    public static String generateSessionCode(){
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String sessionCode = "";
+        for (int i = 0; i < 6; i++){
+            sessionCode += alphabet.charAt(ThreadLocalRandom.current().nextInt(0, alphabet.length()));
+        }
+        return sessionCode;
+    }
 }
