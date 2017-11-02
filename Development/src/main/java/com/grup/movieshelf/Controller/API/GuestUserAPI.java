da21574f8eeb3f45b53d39605bb18a501e3c8629
Chris Dalke <chrisdalke@gmail.com>
2017-11-02 00:34:25 -0400

    Working on adding some of the REST apis

---
+++ Development/src/main/java/com/grup/movieshelf/Controller/API/GuestUserAPI.java
@@ -0,0 +1,25 @@
+package com.grup.movieshelf.Controller.API;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GuestUserAPI {
+
+    @PostMapping("/api/guest")
+    public void createGuest(){
+
+    }
+
+    @GetMapping("/api/guest")
+    public void getGuest(){
+
+    }
+
+
+
+
+
+}
