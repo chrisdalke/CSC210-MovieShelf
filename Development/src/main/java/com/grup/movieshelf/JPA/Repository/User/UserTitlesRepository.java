4d48877868ebfd459f5a67350945b05ae51f41b5
danielstegink <danielstegink@yahoo.com>
2017-11-13 18:35:18 -0500

    Can only add 1 friend - why?
    Fixed it so that one can add and remove friends; friend list displayed below shelf currently; but cannot add more than one friend to list. Still not sure why it works like this. Could be a problem with my connection to SQL?

--- Development/src/main/java/com/grup/movieshelf/JPA/Repository/User/UserTitlesRepository.java
+++ Development/src/main/java/com/grup/movieshelf/JPA/Repository/User/UserTitlesRepository.java
@@ -2,9 +2,7 @@ package com.grup.movieshelf.JPA.Repository.User;
 
 import com.grup.movieshelf.JPA.Entity.Users.UserTitle;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
-
 
 public interface UserTitlesRepository extends JpaRepository<UserTitle, String> {
 
--- Development/src/main/java/com/grup/movieshelf/JPA/Utility/HibernateUserDetailsService.java
+++ Development/src/main/java/com/grup/movieshelf/JPA/Utility/HibernateUserDetailsService.java
@@ -1,11 +1,13 @@
 package com.grup.movieshelf.JPA.Utility;
 
 import com.grup.movieshelf.JPA.Entity.Users.User;
+import com.grup.movieshelf.JPA.Entity.Users.Friendship;
 import com.grup.movieshelf.JPA.Entity.Users.UserOptions;
 import com.grup.movieshelf.JPA.Entity.Users.UserTitle;
 import com.grup.movieshelf.JPA.Repository.User.RoleRepository;
 import com.grup.movieshelf.JPA.Repository.User.UserOptionsRepository;
 import com.grup.movieshelf.JPA.Repository.User.UserRepository;
+import com.grup.movieshelf.JPA.Repository.User.FriendshipRepository;
 import com.grup.movieshelf.JPA.Repository.User.UserTitlesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
@@ -23,6 +25,9 @@ public class HibernateUserDetailsService implements UserDetailsService {
 
    @Autowired
    private UserRepository userRepository;
+
+   @Autowired
+   private FriendshipRepository friendshipRepository;
 
    @Autowired
    private UserOptionsRepository userOptionsRepository;
@@ -63,6 +68,23 @@ public class HibernateUserDetailsService implements UserDetailsService {
         Integer userId = hibernateSecurityService.getLoggedInUser().getUserId();
         UserTitle userTitle = userTitleRepository.getByUserIdAndTitleId(userId, titleId);
         userTitleRepository.delete(userTitle);
+   }
+
+   public void addFriend (String userName){
+      Integer userId = hibernateSecurityService.getLoggedInUser().getUserId();
+      Integer friendId = userRepository.findByUsername(userName).getUserId();
+      String friendship_name = userId+"_"+friendId;
+      if (friendshipRepository.getByFriendshipId(friendship_name) == null) {
+         Friendship friendship = new Friendship(userId, friendId);
+         friendshipRepository.save(friendship);
+      }
+   }
+
+   public void removeFriend (String friendshipId){
+      if (friendshipRepository.getByFriendshipId(friendshipId) != null) {
+         Friendship friendship = friendshipRepository.getByFriendshipId(friendshipId);
+         friendshipRepository.delete(friendship);
+      }
    }
 
    /*
