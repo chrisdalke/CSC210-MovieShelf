04503981d53733809755b3ab2700483d292d1b92
Chris Dalke <chrisdalke@gmail.com>
2017-11-14 13:18:52 -0500

    Commented API controllers and moved functionality into services.

--- Development/src/main/java/com/grup/movieshelf/Controller/API/FriendAPI.java
+++ Development/src/main/java/com/grup/movieshelf/Controller/API/FriendAPI.java
@@ -1,21 +1,34 @@
+///////////////////////////////////////////////////////////////
+// MOVIESHELF
+// CSC 210 Final Project, Fall 2017
+// Chris Dalke, Nate Conroy, Andrew Gutierrez, Daniel Stegink
+///////////////////////////////////////////////////////////////
+
 package com.grup.movieshelf.Controller.API;
 
-import com.grup.movieshelf.Controller.API.Entity.*;
+/////////////////////////////////////////////////////////////
+// Module Imports
+/////////////////////////////////////////////////////////////
+
 import com.grup.movieshelf.Controller.API.Entity.ResponseStatus;
 import com.grup.movieshelf.JPA.Entity.Users.Friendship;
-import com.grup.movieshelf.JPA.Repository.User.FriendshipRepository;
-import com.grup.movieshelf.JPA.Repository.User.UserRepository;
-import com.grup.movieshelf.JPA.Utility.HibernateSecurityService;
-import com.grup.movieshelf.JPA.Utility.HibernateUserDetailsService;
+import com.grup.movieshelf.JPA.Repository.FriendshipRepository;
+import com.grup.movieshelf.JPA.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import com.grup.movieshelf.Service.*;
+
+/////////////////////////////////////////////////////////////
+// Friend API
+// Handles REST endpoints for the Friendship system.
+/////////////////////////////////////////////////////////////
 
 @RestController
 public class FriendAPI {
 
-    @Autowired
-    private HibernateSecurityService hibernateSecurityService;
+    //------------------------------------------------
+    // Dependencies
+    //------------------------------------------------
 
     @Autowired
     private UserRepository userRepository;
@@ -24,28 +37,29 @@ public class FriendAPI {
     private FriendshipRepository friendshipRepository;
 
     @Autowired
-    private HibernateUserDetailsService hibernateUserDetailsService;
+    private UserService userService;
+
+    //------------------------------------------------
+    // Request Mappings
+    //------------------------------------------------
 
     @PostMapping("/api/friends")
     public ResponseStatus addFriend (@RequestParam("userName") String friendUserName) {
-        Integer userId = hibernateSecurityService.getLoggedInUser().getUserId();
-        Integer friendId = userRepository.findByUsername(friendUserName).getUserId();
-        String friendship_name = userId+"_"+friendId;
-        if (friendshipRepository.getByFriendshipId(friendship_name) == null) {
-            Friendship friendship = new Friendship(userId, friendId);
-            friendshipRepository.save(friendship);
-        }
+
+        userService.addFriend(friendUserName);
 
         return new ResponseStatus();
     }
 
     @RequestMapping("/api/friends")
     public ResponseStatus removeFriend (@RequestParam("friendshipId") String friendId) {
-        if (friendshipRepository.getByFriendshipId(friendId) != null) {
-            Friendship friendship = friendshipRepository.getByFriendshipId(friendId);
-            friendshipRepository.delete(friendship);
-        }
+
+        userService.removeFriend(friendId);
 
         return new ResponseStatus();
     }
 }
+
+/////////////////////////////////////////////////////////////
+// End of File
+/////////////////////////////////////////////////////////////
--- Development/src/main/java/com/grup/movieshelf/Controller/API/UserAPI.java
+++ Development/src/main/java/com/grup/movieshelf/Controller/API/UserAPI.java
@@ -1,9 +1,41 @@
+///////////////////////////////////////////////////////////////
+// MOVIESHELF
+// CSC 210 Final Project, Fall 2017
+// Chris Dalke, Nate Conroy, Andrew Gutierrez, Daniel Stegink
+///////////////////////////////////////////////////////////////
+
 package com.grup.movieshelf.Controller.API;
 
+/////////////////////////////////////////////////////////////
+// Module Imports
+/////////////////////////////////////////////////////////////
+
+import com.grup.movieshelf.JPA.Repository.UserOptionsRepository;
+import com.grup.movieshelf.Service.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+/////////////////////////////////////////////////////////////
+// User API
+// REST API for interacting with users.
+/////////////////////////////////////////////////////////////
 
 @RestController
 public class UserAPI {
+
+    //------------------------------------------------
+    // Dependencies
+    //------------------------------------------------
+
+    @Autowired
+    private static UserService userService;
+
+    @Autowired
+    private static UserOptionsRepository userOptionsRepository;
+
+    //------------------------------------------------
+    // REST methods for User
+    //------------------------------------------------
 
     @PostMapping("/api/user")
     public void createUser(){
@@ -29,6 +61,8 @@ public class UserAPI {
     public void getActiveUser(){
 
     }
-
-
 }
+
+/////////////////////////////////////////////////////////////
+// End of File
+/////////////////////////////////////////////////////////////
