47882465b47a3a9349d4abae0a8a0df898adedc8
Chris Dalke <chrisdalke@gmail.com>
2017-11-15 00:04:14 -0500

    Basic live search functionality.

---
+++ Development/src/main/resources/public/js/search.js
@@ -0,0 +1,45 @@
+/**
+ * Created by chrisdalke on 11/14/17.
+ */
+$(function(){
+
+    $( "#search-form" ).submit(function( event ) {
+        event.preventDefault();
+        runSearch();
+    });
+
+    $("#searchString").keyup(function(){
+        runSearch();
+    })
+
+});
+
+var isSearching = false;
+
+function runSearch(){
+    var searchStringValue = $("#searchString").val()
+    if (searchStringValue.length > 0){
+        $("#search-pane").show();
+        if (!isSearching) {
+            isSearching = true;
+            doSearch(searchStringValue, function (data) {
+                parseSearchResults(data);
+                isSearching = false;
+            }, function(){
+                //Search failed, don't parse the results.
+                isSearching = false
+            });
+        }
+    } else {
+        $("#search-pane").hide();
+    }
+}
+
+function parseSearchResults(searchObject){
+    console.log("SEARCH RESULTS:");
+    console.log(searchObject);
+    $("#search-results").html("");
+    for (var i = 0; i < searchObject.length; i++) {
+        $("#search-results").append("<b>"+searchObject[i].titleName+" ("+searchObject[i].year+") </b><br>");
+    }
+}
