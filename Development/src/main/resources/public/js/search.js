8719adc6ee60b9fbaa07f5c447f942c8ffac866b
Andrew Gutierrez <agutier4@u.rochester.edu>
2017-11-18 18:17:02 -0500

    lame

--- Development/src/main/resources/public/js/search.js
+++ Development/src/main/resources/public/js/search.js
@@ -43,6 +43,26 @@ function parseSearchResults(searchObject){
         if(i>10){
             break;
         }
-        $("#search-results").append("<b>"+searchObject[i].titleName+" ("+searchObject[i].year+") </b><br>");
+        var entry = $("<div class=\"uk-grid-match uk-grid-collapse \" uk-grid></div><br>");
+        var titleCard=$("<div class=\"uk-card uk-width-expand@m\">"+searchObject[i].titleName+" ("+searchObject[i].year+")</div>");
+        var buttonCard =$("<div class=\"uk-card addButton uk-width-auto@m\"><a icon: \"plus\"></a></div>");
+        var titleObject=Object.freeze(searchObject[i]);
+        buttonCard.click(function(){
+            addFavorite(titleObject.titleId);
+            console.log("added");
+        });
+        entry.append(titleCard);
+        entry.append(buttonCard);
+        $("#search-results").append(entry);
+        /*
+        var entry = $("<div class=\"uk-card \"></div><br>");
+        var body= $("<div class=\"uk-card-body \">"+searchObject[i].titleName+" ("+searchObject[i].year+")</div>")
+        var right=$("<div class=\"uk-card-media-right\"></div>");
+        var button=$("<a class=\"uk-button uk-button-default\" href=\"/titles/"+searchObject[i].titleId+"\"> View</a>");
+        right.append(button);
+        entry.append(body);
+        entry.append(right);
+        $("#search-results").append(entry);
+        */
     }
 }
