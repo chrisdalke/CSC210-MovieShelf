3a1af789e83ea250888ab97dda09b4a10cb1e3fd
danielstegink <danielstegink@yahoo.com>
2017-11-28 18:55:11 -0500

    Setting up for removeFriend
    Added some javascript to the JS file. Doesn't work currently; need to figure out how to request specific api function, as well as how to pass it the friend's user name.

--- Development/src/main/resources/public/js/friendsPage.js
+++ Development/src/main/resources/public/js/friendsPage.js
@@ -1,3 +1,9 @@
 /**
  * Created by chrisdalke on 11/15/17.
  */
+
+$( "#remove" ).click(function() {
+    $.get( "http://localhost:8080/api/friends", function( data ) {
+      // how to run removeFriend?
+    });
+});
