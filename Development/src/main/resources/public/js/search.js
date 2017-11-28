5a883116bba6b35ea9f5566bb882e9c0fc01dc53
Andrew Gutierrez <agutier4@u.rochester.edu>
2017-11-27 21:52:47 -0500

    Dynamic buttons, fixed contains title, working on fixing button callbacks. Bug currently adds the whole search list...

--- Development/src/main/resources/public/js/search.js
+++ Development/src/main/resources/public/js/search.js
@@ -23,7 +23,7 @@ function runSearch(){
         if (!isSearching) {
             isSearching = true;
             doSearch(searchStringValue, function (data) {
-                parseSearchResults(data);
+                buildResults(data);
                 isSearching = false;
             }, function(){
                 //Search failed, don't parse the results.
@@ -35,55 +35,68 @@ function runSearch(){
     }
 }
 
-function parseSearchResults(searchObject){
-    console.log("SEARCH RESULTS:");
-    console.log(searchObject);
-    $("#search-results").html("");
-    for (var i = 0; i < searchObject.length; i++) {
-        if(i>10){
-            break;
-        }
-        var entry = $("<div class=\"uk-grid-match uk-grid-collapse \" uk-grid></div><br>");
-        var titleCard=$("<div class=\"uk-card uk-width-expand@m\"></div>");
-        var buttonCard;
-        var cardText=$("<a class=\"uk-position-center\" href=\"/titles/"+searchObject[i].titleId+"\">"+searchObject[i].titleName+" ("+searchObject[i].year+")</a>");
-        var titleObject=Object.freeze(searchObject[i]); 
-        
-        if(!containsTitle(searchObject[i].titleId)){
-            buttonCard =$("<div class=\"uk-card addButton uk-width-auto@m\"><span class=\"uk-position-center signs\" uk-icon=\"icon: plus\"></span></div>");
-            buttonCard.click(function(){
-                addFavorite(titleObject.titleId,function(){
-                    refreshShelf();
-                });
-                console.log("added");
-            });
-        }else{
-          buttonCard =$("<div class=\"uk-card removeButton uk-width-auto@m\"><span class=\"uk-position-center signs\" uk-icon=\"icon: minus\"></span></div>");
-            buttonCard.click(function(){
-                removeFavorite(titleObject.titleId,function(){
-                    refreshShelf();
-                });
-                console.log("removed");
-            });  
-        }
-        
-        titleCard.append(cardText);
-        entry.append(titleCard);
-        entry.append(buttonCard);
-        $("#search-results").append(entry);
-    }
-}
-
-function containsTitle(titleId){
-    var bool;
-    bool=false;
+function buildResults(searchObject){
     getLibrary(function(library){
-        for (var i = 0; i < library.length; i++) {
-            if(titleId===library[i].titleId){
-               bool=true;
+        $("#search-results").html("");
+        for (var i = 0; i < searchObject.length; i++){
+            if (i>10)
+                break;
+             var entry = $("<div class=\"uk-grid-match uk-grid-collapse \" uk-grid></div><br>");
+             var titleCard=$("<div class=\"uk-card uk-width-expand@m\"></div>");
+             var buttonCard;
+             var cardText=$("<a class=\"uk-position-center\" href=\"/titles/"+searchObject[i].titleId+"\">"+searchObject[i].titleName+" ("+searchObject[i].year+")</a>");
+             //var titleObject=Object.freeze(searchObject[i]);
+
+             function add(titleId){
+                addFavorite(titleId,function(){
+                        refreshShelf();
+                    });
+                console.log("added");
+             }
+
+             function remove(titleId){
+                removeFavorite(titleId,function(){
+                    refreshShelf();
+                    });
+                console.log("removed");
+             }
+
+             if(!containsTitle(searchObject[i].titleId)){
+                buttonCard =$("<div class=\"uk-card addButton uk-width-auto@m\"><span class=\"uk-position-center signs\" uk-icon=\"icon: plus\"></span></div>");
+                buttonCard.click(add(searchObject[i].titleId));
+                /*
+                buttonCard.click(function(){
+                    addFavorite(titleObject[i].titleId,function(){
+                        refreshShelf();
+                    });
+                console.log("added");
+                });
+                */
+             }else{
+                buttonCard =$("<div class=\"uk-card removeButton uk-width-auto@m\"><span class=\"uk-position-center signs\" uk-icon=\"icon: minus\"></span></div>");
+                buttonCard.click(remove(searchObject[i].titleId));
+                /*
+                buttonCard.click(function(){
+                    removeFavorite(titleObject[i].titleId,function(){
+                    refreshShelf();
+                    });
+                console.log("removed");
+                }); 
+                */
             }
+            titleCard.append(cardText);
+            entry.append(titleCard);
+            entry.append(buttonCard);
+            $("#search-results").append(entry);
+        }
+
+        function containsTitle(titleId){
+            for (var i = 0; i < library.length; i++) {
+                if(titleId===library[i].titleId){
+                    return true;
+                }
+            }
+            return false;
         }
     });
-    console.log(bool);
-    return bool;
 }
