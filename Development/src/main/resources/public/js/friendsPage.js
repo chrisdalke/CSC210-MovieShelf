82c883537041670be3585b40eea4533f26c4453b
danielstegink <danielstegink@yahoo.com>
2017-11-28 19:22:01 -0500

    Updated code
    Still not sure how to run removeFriend from JQuery, but added some code to pass friend user name along.

--- Development/src/main/resources/public/js/friendsPage.js
+++ Development/src/main/resources/public/js/friendsPage.js
@@ -2,8 +2,16 @@
  * Created by chrisdalke on 11/15/17.
  */
 
-$( "#remove" ).click(function() {
-    $.get( "http://localhost:8080/api/friends", function( data ) {
+$( "#remove" ).on ("click", function(event) {
+    //found this function at - https://stackoverflow.com/questions/487056/retrieve-button-value-with-jquery
+    var getButtonValue = function(event) {
+        var label = event.text();
+        event.text('');
+        var buttonValue = event.val();
+        event.text(label);
+        return buttonValue;
+    }
+    $.get( "http://localhost:8080/api/friends", function( getButtonValue ) {
       // how to run removeFriend?
     });
 });
