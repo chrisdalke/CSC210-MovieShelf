3013ee818e89e220004a6fd6a03c1d5494bc81db
danielstegink <danielstegink@yahoo.com>
2017-11-30 11:19:28 -0500

    Re-drafted so JS -> API -> Service -> Database; still draft code

--- Development/src/main/java/com/grup/movieshelf/Controller/API/MetadataAPI.java
+++ Development/src/main/java/com/grup/movieshelf/Controller/API/MetadataAPI.java
@@ -4,42 +4,49 @@
 // Chris Dalke, Nate Conroy, Andrew Gutierrez, Daniel Stegink
 ///////////////////////////////////////////////////////////////
 
-package com.grup.movieshelf.Service;
+package com.grup.movieshelf.Controller.API;
 
 /////////////////////////////////////////////////////////////
 // Module Imports
 /////////////////////////////////////////////////////////////
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.grup.movieshelf.Service.*;
+import java.util.List;
 
 /////////////////////////////////////////////////////////////
-// Metadata Service
-// Functionality for Movie Meta Data
+// Search API
+// REST Controller handling getImage and addMetadata functionality
 /////////////////////////////////////////////////////////////
 
-@Service
-public class MetadataService {
+@RestController
+public class MetadataAPI {
 
     //------------------------------------------------
     // Dependencies
     //------------------------------------------------
 
+    @Autowired
+    private MetadataService metadataService;
+
     //------------------------------------------------
-    // Add Metadata and Get Movie Image
+    // Request Mappings
     //------------------------------------------------
 
-    public String getImage (String titleId){
-        // returns image url from database
+    @GetMapping("/api/meta")
+    public String getMovieImage(String titleId) {
+        if (metadataService.contains(titleId)) { return metadataService.getImage(titleId); }
+        else { // if not, call api.js addMetadata
+            return metadataService.getImage(titleId);
+        }
     }
 
-    public boolean contains(String titleId){
-        // check if database contains titleId
-    }
-
-    public void addMetadata(String img_url, String description) {
-        // add img_url to database
-        // add description to database
+    @PostMapping("/api/meta/...")
+    public void addMetadata(String img_url, String description){
+        // calls service - tells it to add the params to databasse
     }
 }
 
