a9333e101d00916bf382ea69d739ee8d6c63e0a2
danielstegink <danielstegink@yahoo.com>
2017-11-30 11:41:47 -0500

    Revert "Added code for handling metadata"
    This reverts commit 8fa3b81494fc601b813bf05e5d4906f43e70c87a.

--- Development/src/main/resources/public/js/api.js
+++ Development/src/main/resources/public/js/api.js
@@ -82,29 +82,11 @@ function addFavorite(title,success){
 
 function removeFavorite(title,success){
     doAjax("/api/shelf/"+title,"DELETE",{},success,null);
+
 }
 
 function getFavorites(){
 
-}
-
-//------------------------------------------------
-// Metadata API
-//------------------------------------------------
-function addMetadata(titleId,success){
-    try{
-        UserAgent userAgent = new UserAgent();
-        String url = "http://imdb.com/title/" + titleId;
-        userAgent.visit(url);
-        String img_url = userAgent.doc.findFirst("<div class=\"poster\">").findFirst("<img>").getAt("src");
-        String description = userAgent.doc.findFirst("<div class=\"summary_text\">").getText();
-        // make call to api to add img_url and description to database
-        // doAjax("/api/../"+titleId+"/"+img_url+"/"+description, "POST", { }, success,null);
-        // instead of dealing with multiple functions, make one that takes both? is this possible?
-    }
-    catch(JauntException e){
-        System.err.println(e);
-    }
 }
 
 //------------------------------------------------
