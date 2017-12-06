419d1cf451739f7cd0a763c88988fd306d851179
Nate Conroy <nconroy@u.rochester.edu>
2017-12-06 11:37:53 -0500

    fixed syntax errors in shelf.js

--- Development/src/main/resources/public/js/shelf.js
+++ Development/src/main/resources/public/js/shelf.js
@@ -5,14 +5,15 @@ $(function(){
 function refreshShelf(){
     doAjax("/api/shelf","GET",{},function(data){
         var shelfDivString = "";
+        var input;
         for (var i = 0; i < data.length; i +=3){
             //Add a shelf row
             shelfDivString += "<div class=\"shelf\"><div class=\"shelf-grid uk-grid-collapse uk-child-width-expand@s\" uk-grid>";
-            if (i + 0 < data.length){
-                shelfDivString += "<div class=\"shelf-item-holder\"><div class=\"shelf-item-box\"><a href=\"/titles/"+data[i+0].titleId+"\"><div class=\"movie\">";
+            if (i < data.length){
+                shelfDivString += "<div class=\"shelf-item-holder\"><div class=\"shelf-item-box\"><a href=\"/titles/"+data[i].titleId+"\"><div class=\"movie\">";
                 //shelfDivString += data[i+0].titleName;
                 //shelfDivString += " ("+data[i+0].year+")";
-                String input = "/api/meta/"+data[i+0].titleId;
+                input = "/api/meta/"+data[i].titleId;
                 doAjax(input, "GET", {}, function(img_src) {
                     shelfDivString += "<img src=\"";
                     shelfDivString += img_src;
@@ -26,7 +27,7 @@ function refreshShelf(){
                 shelfDivString += "<div class=\"shelf-item-holder\"><div class=\"shelf-item-box\"><a href=\"/titles/"+data[i+1].titleId+"\"><div class=\"movie\">";
                 //shelfDivString += data[i+1].titleName;
                 //shelfDivString += " ("+data[i+1].year+")";
-                String input = "/api/meta/"+data[i+1].titleId;
+                input = "/api/meta/"+data[i+1].titleId;
                                 doAjax(input, "GET", {}, function(img_src) {
                                     shelfDivString += "<img src=\"";
                                     shelfDivString += img_src;
@@ -41,7 +42,7 @@ function refreshShelf(){
                 shelfDivString += "<div class=\"shelf-item-holder\"><div class=\"shelf-item-box\"><a href=\"/titles/"+data[i+2].titleId+"\"><div class=\"movie\">";
                 //shelfDivString += data[i+2].titleName;
                 //shelfDivString += " ("+data[i+2].year+")";
-                String input = "/api/meta/"+data[i+2].titleId;
+                input = "/api/meta/"+data[i+2].titleId;
                                 doAjax(input, "GET", {}, function(img_src) {
                                     shelfDivString += "<img src=\"";
                                     shelfDivString += img_src;
