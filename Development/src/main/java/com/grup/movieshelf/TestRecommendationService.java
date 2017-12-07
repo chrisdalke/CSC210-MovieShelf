f603eadd447771e3cc76697ffffee35a00662b3b
Nate Conroy <nconroy@u.rochester.edu>
2017-12-07 01:09:15 -0500

    added annotation so that recommendations test runs

--- Development/src/main/java/com/grup/movieshelf/TestRecommendationService.java
+++ Development/src/main/java/com/grup/movieshelf/TestRecommendationService.java
@@ -7,7 +7,10 @@ import com.grup.movieshelf.Service.ShelfService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+@Component
 public class TestRecommendationService implements ApplicationListener<ApplicationReadyEvent> {
 
     @Autowired
@@ -17,6 +20,7 @@ public class TestRecommendationService implements ApplicationListener<Applicatio
     RecommendationService recommendationService;
 
     @Override
+    @Transactional
     public void onApplicationEvent(final ApplicationReadyEvent event) {
         // Test recommendation algorithm with Nate's shelf as the input
         RecommendationInput recommendationInput = new RecommendationInput(shelfService.getShelfForUser(9));
